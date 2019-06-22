81c3e5da18128d14ae221b627e5d1b98ddfa1027
Becky <46365521+Becky-nuo@users.noreply.github.com>
2019-06-22 20:10:03 +0800

    feat:add files

---
+++ Day1-15/Day05/03.py
@@ -0,0 +1,8 @@
+dict = {'Name': 'chen', 'Age': 18, 'Class': 'First'}
+ 
+dict['Age'] = 19 # 更新
+dict['School'] = "JXYY" # 添加
+ 
+ 
+print ("dict['Age']:  ",dict['Age'])
+print ("dict['School']: ", dict['School'])
