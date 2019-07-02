4d47f0cadf56862ccf8028ad693b5d67d6014906
Becky <46365521+Becky-nuo@users.noreply.github.com>
2019-07-02 20:12:26 +0800

    feat:add files

---
+++ Day1-15/Day05/04.py
@@ -0,0 +1,12 @@
+
+
+
+import os, sys
+
+#字典
+dict={}
+dict["name"]="初学"
+dict["say"]="晚上好"
+dict["detail"]="每天一练"
+for k in dict.keys():
+    print(k)
---
+++ Day1-15/Day07/08.py
@@ -0,0 +1,5 @@
+#无限循环
+b=1
+while b == 1 :  #表达式为true
+    b=int(input("输入一个数字 ："))
+    print("输入的数字是: ",b)
